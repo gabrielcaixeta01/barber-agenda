3af3d83dba41b92a4e2672735959eaef2607b84c
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2026-02-02 16:06:23 -0300

    fix: erro no login por causa do service.ts ajustado

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,11 +4,11 @@ import { createServerClient } from "@supabase/ssr";
 export async function createSupabaseServer() {
   const cookieStore = await cookies();
 
-  const url = process.env.SUPABASE_URL;
-  const key = process.env.SUPABASE_ANON_KEY;
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   if (!url || !key) {
-    throw new Error("Missing env: SUPABASE_URL / SUPABASE_ANON_KEY");
+    throw new Error("Missing env: NEXT_PUBLIC_SUPABASE_URL / NEXT_PUBLIC_SUPABASE_ANON_KEY");
   }
 
   return createServerClient(url, key, {
